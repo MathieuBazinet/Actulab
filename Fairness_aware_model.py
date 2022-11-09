a88a244bf56327a469afade6ad4ee898bc445fa9
Mathieu Bazinet <mathieu.bazinet.2@ulaval.ca>
2022-11-09 14:16:55 -0500

    Première version d'un modèle fonctionnel

--- Fairness_aware_model.py
+++ Fairness_aware_model.py
@@ -1,25 +1,81 @@
 import numpy as np
-import scipy.optimize
+from scipy.optimize import minimize
+from scipy.special import factorial
 import pandas as pd
 from sklearn.utils import check_random_state
 
+
 class FairnessAwareModel:
 
-    def __init__(self, regularization, link_function, seed=42):
-        self.reg = regularization
-        self.link_function = link_function
+    def __init__(self, regularization, protected_attributes, offset=None, beta_init=None,  seed=42):
         # On verra si on a besoin de random states mais je les mets là au cas où
+
+        # On crée une liste d'attributs protégés
+        if not isinstance(protected_attributes, list):
+            self.protected_attributes = [protected_attributes]
+        else:
+            self.protected_attributes = protected_attributes
+
+        # On attribue un paramètre de régularisation par attribut protégé
+        if not isinstance(regularization, list):
+            self.regularization = [regularization] * len(self.protected_attributes)
+        else:
+            if len(regularization) != len(self.protected_attributes):
+                raise AssertionError("Le nombre de paramètres de régularisation " + \
+                                     "n'est pas égal au nombre d'attributs protégés")
+            self.regularization = regularization
+
         self.random_state = check_random_state(seed)
         self.np_random_state = np.random.RandomState(seed)
+        self.beta = None
+        self.beta_init = beta_init
+        self.offset = offset
 
-    def predict(self):
-        pass
+    def beta_dot(self, X):
+        if self.beta is None or self.offset is None:
+            raise RuntimeError("The model was not fitted on the data.")
+        return X @ self.beta + np.log(self.offset)
+
+    def predict(self, X):
+        return np.exp(self.beta_dot(X))
+
+    def log_vraisemblance_poisson(self, X, y):
+        predicted_x = self.beta_dot(X).reshape(-1, 1)
+        y = y.reshape(-1, 1)
+        yx = np.sum(y * predicted_x - np.exp(predicted_x))
+        c = np.sum(-np.log(factorial(y)))
+        return yx + c
+
+    def penalized_loss(self, beta):
+        self.beta = beta
+        self.beta = self.beta / np.linalg.norm(self.beta)
+        print(self.beta)
+        log_vraisemblance = self.log_vraisemblance_poisson(self.X, self.y)
+        loss = 0
+        for s_index in range(len(self.protected_attributes)):
+            s = self.protected_attributes[s_index]
+            predict_list = []
+            regularization_parameter = self.regularization[s_index]
+            for a in np.unique(self.X[:, s]):
+                X_a = self.X
+                X_a[:, s] = a
+                predict_list.append(self.predict(X_a))
+            for i in range(len(predict_list)):
+                for j in range(i, len(predict_list)):
+                    loss += regularization_parameter * np.sum((predict_list[i] - predict_list[j])**2)
+        return -log_vraisemblance + loss
 
     def fit(self, X_train, y_train):
-        pass
-
-    def custom_loss(self):
-        pass
+        self.X = X_train
+        self.y = y_train
+        if self.beta_init is None:
+            self.beta_init = np.ones(X_train.shape[1]) / X_train.shape[1]
+        if self.offset is None:
+            self.offset = np.ones(X_train.shape[0])
+        res = minimize(self.penalized_loss, self.beta_init, method='BFGS', options={'maxiter': 500})
+        self.beta = res.x
+        self.beta = self.beta / np.linalg.norm(self.beta)
+        self.beta_init = self.beta
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import numpy as np
 import pandas as pd
+from Fairness_aware_model import FairnessAwareModel
+
+
+protected_attributes = ['gender']
+
+# Standard scaling for regression
 
 dataCar = pd.read_csv("./dataCar.csv")
-dataCar = dataCar.drop("X_OBSTAT_", axis=1)
+binary_answer = dataCar["clm"].values
+numclaim = dataCar["numclaims"].values
+reg_claim = dataCar["claimcst0"].values
+
+dataCar = dataCar.drop(["X_OBSTAT_", "clm", "numclaims", "claimcst0"], axis=1)
+
 # TODO encoder correctement les différentes valeurs qui ne sont pas des entiers
-# dataCar.replace(to_replace=np.unique(dataCar['gender']), inplace=True, value=range(np.unique(dataCar['gender']).shape[0]))
-# dataCar.replace(to_replace=np.unique(dataCar['veh_body']), inplace=True, value=range(np.unique(dataCar['veh_body']).shape[0]))
-# dataCar.replace(to_replace=np.unique(dataCar['area']), inplace=True, value=range(np.unique(dataCar['area']).shape[0]))
+dataCar['gender'].replace(to_replace=np.unique(dataCar['gender']), inplace=True, value=range(np.unique(dataCar['gender']).shape[0]))
+dataCar['veh_body'].replace(to_replace=np.unique(dataCar['veh_body']), inplace=True, value=range(np.unique(dataCar['veh_body']).shape[0]))
+dataCar['area'].replace(to_replace=np.unique(dataCar['area']), inplace=True, value=range(np.unique(dataCar['area']).shape[0]))
+
+data = dataCar.values
+protected_attributes = [np.where(dataCar.columns.values == i)[0][0] for i in protected_attributes]
+
+fam = FairnessAwareModel(regularization=0.1, protected_attributes=protected_attributes)
+fam.fit(data, numclaim)
+results = fam.predict(data)
