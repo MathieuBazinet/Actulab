5ce0251ddd66d6376cb01ad82b99dba212a8863f
Simon <simon-olivier.lepine.1@ulaval.ca>
2022-11-09 15:20:56 -0500

    exploration régression logistique

--- exploration.R
+++ exploration.R
@@ -2,7 +2,7 @@ library(tidyverse);library(MASS)
 
 df = read.csv("dataCar.csv")
 df = df %>%
-  select(-X_OBSTAT_) %>%
+  dplyr::select(-c(X_OBSTAT_)) %>%
   mutate(
     veh_body = as.factor(veh_body),
     veh_age = as.factor(veh_age),
@@ -15,50 +15,77 @@ df = df %>%
 # Statistiques descriptives
 ################################################################################
 
-round(df$numclaims %>% table() %>% prop.table()*100,2)
+round(df$numclaims %>% table() %>% prop.table()*100,2) %>% cumsum()
+# 99.58% des données ont 0 ou 1 réclamation. Dans ce contexte, il est approprié
+# d'utiliser une variable binaire, c'est-à-dire considérer s'il y a au moins une
+# réclamation dans l'année. Le GLM sera donc une régression logistique.
+# Nous utiliserons comme variable y la variable `clm`.
 
 ################################################################################
-# Attributs protégés
+# Attributs protégés choisis
 ################################################################################
 # gender
 # agecat
 
 ################################################################################
-# exposition
+# Régression logistique
 ################################################################################
-# la variable exposure correspond à la proportion de l'année pendant
-# laquelle l'assuré(e) est couvert(e).
-# (http://cas.uqam.ca/pub/web/CASdatasets-manual.pdf)
-# Plus l'individu est assuré # longtemps, plus il y a possiblement de 
-# réclamations (proportionnel, # donc on va avoir une variable d'offset)
-# Nous incluons cette variable comme un terme d'offset.
-# Nous créons donc la variable log_Exposure
-df$log_exposure <- log(df$exposure) # log=ln
+formule = formula(clm~veh_value+veh_body+veh_age+area)
+
+library(caTools)
+set.seed(42)
+sample = sample.split(df$clm, SplitRatio = .8)
+train = subset(df, sample == TRUE)
+test  = subset(df, sample == FALSE)
+
+logistique = glm(formule, family=binomial(link="logit"), data=train)
+summary(logistique)
+
+test$probs_logit = predict(logistique, newdata=test, type="response")
 
 ################################################################################
-# Poisson ou binomiale négative?
+# XGBoost
 ################################################################################
-formule = formula(numclaims~veh_value+veh_body+veh_age+area+offset(log_exposure))
+library(xgboost)
+library(Matrix)
+train_matrix = sparse.model.matrix(~clm+veh_value+veh_body+veh_age+area, data=train) # sparse.model.matrix ou model.matrix
 
-test_surdispersion = function(formula, dataset){
-  poisson = glm(formula, family=poisson(link="log"), data=dataset)
-  bn = glm.nb(formula, data=dataset)
-  
-  1/2*(1-pchisq(bn$deviance-poisson$deviance, df=1, lower.tail=F))
-}
+xgb <- xgboost(data = train_matrix, label = train$clm, max.depth = 2, eta = 1, nthread = 2, nrounds = 2, objective = "binary:logistic")
 
-### Poisson
-test_surdispersion(formula, df)
-# on rejette H0 donc il y a de l'évidence qu'il y a de la surdispersion.
+test_matrix = sparse.model.matrix(~clm+veh_value+veh_body+veh_age+area, data=test)
 
+test$probs_xgb = predict(xgb, test_matrix)
 
-################################################################################
-# Modèle de Poisson
-################################################################################
-poisson = glm(formula, family=poisson(link="log"), data=df)
-summary(poisson)
+hist(test$probs_xgb)
+hist(test$probs_logit)
 
 ################################################################################
 # Équité dans le modèle de Poisson
 ################################################################################
 library(fairness)
+
+# égalité des chances (equal_odds dans la librairie)
+mean_F = mean(test$probability[test$gender=="F"])
+mean_M = mean(test$probability[test$gender=="M"])
+mean(test$probability)
+
+equal_odds_logit = equal_odds(
+  data    = test, 
+  outcome = 'clm',
+  outcome_base = '0',
+  group   = 'gender',
+  base    = 'F',
+  probs   = 'probability', 
+  cutoff  = mean(test$probs_logit)) # TODO : comment choisir un bon cutoff?
+
+equal_odds_xgb = equal_odds(
+  data    = test, 
+  outcome = 'clm',
+  outcome_base = '0',
+  group   = 'gender',
+  base    = 'F',
+  probs   = 'probability', 
+  cutoff  = mean(test$probs_xgb))
+
+equal_odds_xgb$Metric_plot
+equal_odds_logit$Metric_plot
