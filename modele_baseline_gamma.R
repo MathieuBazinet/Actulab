4589d2793b2659f94a8409975e72b01601df26bb
Simon <simon-olivier.lepine.1@ulaval.ca>
2022-11-16 17:57:09 -0500

    fonction pour créer les graphiques binaires en R

---
+++ modele_baseline_gamma.R
@@ -0,0 +1,55 @@
+library(tidyverse);library(MASS);library(fairness)
+
+df = read.csv("dataCar.csv")
+df = df %>%
+  dplyr::select(-c(X_OBSTAT_)) %>%
+  mutate(
+    veh_body = as.factor(veh_body),
+    veh_age = as.factor(veh_age),
+    gender = as.factor(gender),
+    area = as.factor(area),
+    agecat = as.factor(agecat)
+  )
+
+################################################################################
+# Train-test spit et exporter les données pour Python
+################################################################################
+
+library(caTools)
+set.seed(42)
+sample = sample.split(df$clm, SplitRatio = .8)
+train = subset(df, sample == TRUE)
+test  = subset(df, sample == FALSE)
+
+df$train = sample
+#write.csv(df, "dataCar_clean.csv",row.names=FALSE)
+
+################################################################################
+# Régression gamma
+################################################################################
+# juste pour voir le genre de sortie et les paramètres estimés par la fonction
+df_claimsg0 = subset(df, subset=claimcst0>0)
+
+gam = glm(claimcst0 ~ factor(agecat) + gender + area, data=df_claimsg0, family = Gamma(link="log"))
+summary(gam)
+
+gam$coefficients
+gam$effects
+gam$method
+
+### Estimation du paramètre de dispersion (shape, alpha=nu)
+### remarque : dispersion = phi = nu^-1
+# https://stats.stackexchange.com/questions/367560/how-to-choose-shape-parameter-for-gamma-distribution-when-using-generalized-line
+# https://courses.ms.ut.ee/MTMS.01.011/2019_spring/uploads/Main/GLM_slides_5_continuous_response_print.pdf
+
+summary(gam)$disp # estimation de alpha=nu=2.893
+MASS::gamma.shape(gam) # 0.7632
+# pas du tout la même chose...
+
+?MASS:::gamma.shape.glm
+
+#Dispersion parameter estimation
+#1.Deviance method
+(gam$deviance/gam$df.residual)  # 1.569
+#2.Pearson method
+sum(resid(gam,type='pear')^2)/gam$df.residual # estimation used in summary(model)=2.893
--- modele_baseline_logistique.R
+++ modele_baseline_logistique.R
@@ -1,4 +1,4 @@
-library(tidyverse);library(MASS)
+library(tidyverse);library(MASS);library(fairness);library(latex2exp)
 
 df = read.csv("dataCar.csv")
 df = df %>%
@@ -43,36 +43,216 @@ round(df$numclaims %>% table() %>% prop.table()*100,2) %>% cumsum()
 ################################################################################
 # Régression logistique
 ################################################################################
-formule = formula(clm~veh_value+veh_body+veh_age+area+gender+agecat)
+# discrimination directe
+direct = glm(clm~veh_value+veh_body+veh_age+area+gender, family=binomial(link="logit"), data=train)
+summary(direct)
 
-logistique = glm(formule, family=binomial(link="logit"), data=train)
-summary(logistique)
+# discrimination indirecte
+indirect = glm(clm~veh_value+veh_body+veh_age+area, family=binomial(link="logit"), data=train)
 
-test$probs_logit = predict(logistique, newdata=test, type="response")
-
-################################################################################
-# XGBoost
-################################################################################
-library(xgboost)
-library(Matrix)
-train_matrix = sparse.model.matrix(~clm+veh_value+veh_body+veh_age+area+gender+agecat, data=train) # sparse.model.matrix ou model.matrix
-
-xgb <- xgboost(data = train_matrix, label = train$clm, max.depth = 2, eta = 1, nthread = 2, nrounds = 2, objective = "binary:logistic")
-
-test_matrix = sparse.model.matrix(~clm+veh_value+veh_body+veh_age+area+gender+agecat, data=test)
-
-test$probs_xgb = predict(xgb, test_matrix, type="response")
-
-preds_xgb = test$probs_xgb > 0.5
-
-mean(test$clm==preds_xgb) # accuracy de 100%, le modèle XGBoost classifie parfaitement. Les métriques d'inéquité ne seront pas intéressantes pour ce modèle.
-
-hist(test$probs_xgb) # très (trop?) bon pour classer?
-hist(test$probs_logit)
-
+test$probs_direct = predict(direct, newdata=test, type="response")
+test$probs_indirect = predict(indirect, newdata=test, type="response")
+cutoff = mean(train$clm)
 
 #### GRAPHIQUES POUR LE SEXE
 
+################################################################################
+# Égalité des chances
+# P(\hat{Y}=1 | Y=1,A=a) = P(\hat{Y}=1 | Y=1,A=B) pour tout a,b
+################################################################################
+# égalité des chances (equal_odds dans la librairie)
+mean_F = mean(test$probs_logit[test$gender=="F"])
+mean_M = mean(test$probs_logit[test$gender=="M"])
+mean(test$probs_logit)
+
+# Y=1
+equal_odds_1_direct = equal_odds(
+  data    = test, 
+  outcome = 'clm',
+  outcome_base = '0',
+  group   = 'gender',
+  base    = 'F',
+  probs   = 'probs_direct', 
+  cutoff  = cutoff)
+
+equal_odds_1_direct$Metric_plot + ggtitle("Égalité des chances avec la régression logistique")
+
+# Y=1
+equal_odds_1_indirect = equal_odds(
+  data    = test, 
+  outcome = 'clm',
+  outcome_base = '0',
+  group   = 'gender',
+  base    = 'F',
+  probs   = 'probs_indirect',
+  cutoff  = cutoff)
+
+equal_odds_1_indirect$Metric_plot + ggtitle("Égalité des chances avec la régression logistique")
+
+
+# Y=0
+equal_odds_0_direct = equal_odds(
+  data    = test, 
+  outcome = 'clm',
+  outcome_base = '0',
+  group   = 'gender',
+  base    = 'F',
+  probs   = 1-test$probs_direct, 
+  cutoff  = 1-cutoff)
+
+equal_odds_0_direct$Metric_plot + ggtitle("Égalité des chances avec la régression logistique")
+
+# Y=0
+equal_odds_0_indirect = equal_odds(
+  data    = test, 
+  outcome = 'clm',
+  outcome_base = '0',
+  group   = 'gender',
+  base    = 'F',
+  probs   = 1-test$probs_indirect,
+  cutoff  = 1-cutoff)
+
+equal_odds_0_indirect$Metric_plot + ggtitle("Égalité des chances avec la régression logistique")
+
+
+### Créer graphiques
+max_ratio = function(fairness_output){
+  ratio = fairness_output$Metric[1,1]/fairness_output$Metric[1,2]
+  if(ratio > 1){
+    ratio = 1/ratio
+  }
+  # ratio est entre 0 et 1, ne doit pas être inférieur à 0.8 pour éviter un 
+  # "effet disproportionné".
+  # Philippe Besse. Détecter, évaluer les risques des impacts discriminatoires des algorithmes d'IA. 2020. hal-02616963
+  return(ratio)
+}
+
+max_ratio(equal_odds_0_direct)
+
+object_name_as_str = function(objet){
+    deparse(substitute(objet))
+}
+
+
+object_name_as_str(equal_odds_0_indirect)
+
+create_df = function(...){
+  #' ... est un ensemble d'objets retournées par fairness
+  #' Retourne un dataset qui sera utilisé pour les graphiques
+  
+  df = data.frame()
+  object_names=sapply(substitute(list(...))[-1], deparse)
+  
+  arguments <- list(...)
+  # création du dataset pour chaque modèle testé
+  i=1
+  for(objet in arguments){
+    name = object_names[i]
+
+    if (stringr::str_detect(name, "equal_odds")){
+      metric = "Equalized odds"
+    }
+    
+    if (stringr::str_detect(name, "1")){
+      metric = paste0(metric, ", Y=1")
+    } else{
+      metric = paste0(metric, ", Y=0")
+    }
+    
+    if (stringr::str_detect(name, "indirect")){
+      model = "Indirecte"
+    } else if (stringr::str_detect(name, "penalized")){
+      model = "Pénalité"
+    } else{
+      model = "Directe"
+    }
+    
+    df = rbind(df, c(model, metric, max_ratio(objet)))
+    i = i+1
+  }
+  colnames(df) = c("Modèle", "Métrique", "Ratio.max")
+  df$Ratio.max = as.numeric(df$Ratio.max)
+  
+  return(df)
+}
+
+create_equity_plot = function(dataset, title){
+  scaleFUN <- function(x) sprintf("%.1f", x) # round y tickslabel to 1 decimal
+  
+  ggplot(dataset,
+         aes(x = `Modèle`,
+             y = `Ratio.max`,
+             fill = `Métrique`)) +
+    geom_bar(stat = "identity", # y is actual bar height
+             position = position_dodge()) + # unstack bars
+    labs(y="Ratio maximal", title=TeX(title)) +
+    theme_bw() + 
+    geom_hline(yintercept=1.25, col="red") +
+    scale_y_continuous(labels=scaleFUN)
+      
+}
+
+df = create_df(equal_odds_1_direct, equal_odds_1_indirect, equal_odds_0_direct, equal_odds_0_indirect)
+create_equity_plot(df, "Equalized odds du genre selon le modèle utilisé")
+
+
+
+
+
+
+
+
+
+
+### TODO : Finir la fonction pour générer automatiquement le dataset
+
+### TODO : préparer les graphiques pour la loi gamma en python probablement
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#######
+# OLD
+########
 ################################################################################
 # Parité démographique
 # P(\hat{Y}=1 | S=s) = P(\hat{Y}=1 |S!=s) pour tout y, A et b
@@ -137,26 +317,6 @@ fpr_parity_logit = fpr_parity(
 acc_parity_logit
 fnr_parity_logit
 fpr_parity_logit
-
-################################################################################
-# Égalité des chances
-# P(\hat{Y}=1 | Y=1,A=a) = P(\hat{Y}=1 | Y=1,A=B) pour tout a,b
-################################################################################
-# égalité des chances (equal_odds dans la librairie)
-mean_F = mean(test$probs_logit[test$gender=="F"])
-mean_M = mean(test$probs_logit[test$gender=="M"])
-mean(test$probs_logit)
-
-equal_odds_logit = equal_odds(
-  data    = test, 
-  outcome = 'clm',
-  outcome_base = '0',
-  group   = 'gender',
-  base    = 'F',
-  probs   = 'probs_logit', 
-  cutoff  = mean(test$probs_logit)) # TODO : comment choisir un bon cutoff?
-
-equal_odds_logit$Metric_plot + ggtitle("Égalité des chances avec la régression logistique")
 
 ################################################################################
 # Égalité de la précision
